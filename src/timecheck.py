1a62514c4bdf323e309864ab618dac6fa668349d
avery-crudeman <sphparks@gmail.com>
2015-04-02 05:13:12 -0400

    Update timecheck.py

--- src/timecheck.py
+++ src/timecheck.py
@@ -5,47 +5,52 @@ import time
 from datetime import datetime
 import simplejson as json
 
+
 def endofdaycheck():
-	today = datetime.today()
-	while True:
-		check = datetime.today()
-		if today.day != check.day:
-                        print datetime.strftime(check, "%d %I:%M %p")
-			print "NEW DAY"
-			return
-		else:
-			print "Last time check: " + datetime.strftime(check, "%d %I:%M %p")
-			time.sleep(600)
-			
+    today = datetime.today()
+    while True:
+        check = datetime.today()
+        if today.day != check.day:
+            print datetime.strftime(check, "%d %I:%M %p")
+            print "NEW DAY"
+            return
+        else:
+            print "Last time check: " + datetime.strftime(check, "%d %I:%M %p")
+            time.sleep(600)
+
+
 def gamecheck(dir):
-	while True:
-		try:
-			response = urllib2.urlopen(dir + "linescore.json")
-			break
-		except:
-                        print datetime.strftime(check, "%d %I:%M %p")
-			print "gamecheck couldn't find file, trying again..."
-			time.sleep(20)
-	jsonfile = json.load(response)
-	game = jsonfile.get('data').get('game')
-	timestring = game.get('time_date') + " " + game.get('ampm')
-	date_object = datetime.strptime(timestring, "%Y/%m/%d %I:%M %p")
-	while True:
-		check = datetime.today()
-		#if (date_object - check).seconds <= 7200:
-		if (date_object - check).seconds <= 10800:
-			return
-		else:
-			print "Last game check: " + datetime.strftime(check, "%d %I:%M %p")
-			time.sleep(600)
-			
+    while True:
+        try:
+            response = urllib2.urlopen(dir + "linescore.json")
+            break
+        except:
+            check = datetime.today()
+            print datetime.strftime(check, "%d %I:%M %p")
+            print "gamecheck couldn't find file, trying again..."
+            time.sleep(20)
+    jsonfile = json.load(response)
+    game = jsonfile.get('data').get('game')
+    timestring = game.get('time_date') + " " + game.get('ampm')
+    date_object = datetime.strptime(timestring, "%Y/%m/%d %I:%M %p")
+    while True:
+        check = datetime.today()
+        if (date_object - check).seconds <= 7200:
+        #if (date_object - check).seconds <= 86400:
+            return
+        else:
+            print "Last game check: " + datetime.strftime(check, "%d %I:%M %p")
+            time.sleep(600)
+
+
 def ppcheck(dir):
-	try:
-		response = urllib2.urlopen(dir + "linescore.json")
-	except:
-                print datetime.strftime(check, "%d %I:%M %p")
-		print "ppcheck Couldn't find file, trying again..."
-		time.sleep(20)
-	jsonfile = json.load(response)
-	game = jsonfile.get('data').get('game')
-	return (game.get('status') == "Postponed")
+    try:
+        response = urllib2.urlopen(dir + "linescore.json")
+    except:
+        check = datetime.today()
+        print datetime.strftime(check, "%d %I:%M %p")
+        print "ppcheck Couldn't find file, trying again..."
+        time.sleep(20)
+    jsonfile = json.load(response)
+    game = jsonfile.get('data').get('game')
+    return (game.get('status') == "Postponed")
