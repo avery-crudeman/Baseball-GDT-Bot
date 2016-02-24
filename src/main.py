835a4f3b8ecc964f23705ef7465073130f222354
avery-crudeman <sphparks@gmail.com>
2016-02-24 03:20:01 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -28,13 +28,30 @@ class Bot:
         self.PASSWORD = None
         self.SUBREDDIT = None
         self.TEAM_CODE = None
+        self.PREGAME_THREAD = None
         self.POST_GAME_THREAD = None
         self.STICKY = None
-        self.POST_SETTINGS = None
+        self.SUGGESTED_SORT = None
+        self.MESSAGE = None
+        self.PRE_THREAD_SETTINGS = None
+        self.THREAD_SETTINGS = None
+        self.POST_THREAD_SETTINGS = None
 
     def read_settings(self):
         with open('settings.json') as data:
             settings = json.load(data)
+
+            self.CLIENT_ID = settings.get('CLIENT_ID')
+            if self.CLIENT_ID == None: return "Missing CLIENT_ID"
+
+            self.CLIENT_SECRET = settings.get('CLIENT_SECRET')
+            if self.CLIENT_SECRET == None: return "Missing CLIENT_SECRET"
+
+            self.REDIRECT_URI = settings.get('REDIRECT_URI')
+            if self.REDIRECT_URI == None: return "Missing REDIRECT_URI"
+
+            self.REFRESH_TOKEN = settings.get('REFRESH_TOKEN')
+            if self.REFRESH_TOKEN == None: return "Missing REFRESH_TOKEN"             
 
             self.BOT_TIME_ZONE = settings.get('BOT_TIME_ZONE')
             if self.BOT_TIME_ZONE == None: return "Missing BOT_TIME_ZONE"
@@ -45,17 +62,14 @@ class Bot:
             self.POST_TIME = settings.get('POST_TIME')
             if self.POST_TIME == None: return "Missing POST_TIME"
 
-            self.USERNAME = settings.get('USERNAME')
-            if self.USERNAME == None: return "Missing USERNAME"
-
-            self.PASSWORD = settings.get('PASSWORD')
-            if self.PASSWORD == None: return "Missing PASSWORD"
-
             self.SUBREDDIT = settings.get('SUBREDDIT')
             if self.SUBREDDIT == None: return "Missing SUBREDDIT"
 
             self.TEAM_CODE = settings.get('TEAM_CODE')
             if self.TEAM_CODE == None: return "Missing TEAM_CODE"
+
+            self.PREGAME_THREAD = settings.get('PREGAME_THREAD')
+            if self.PREGAME_THREAD == None: return "Missing PREGAME_THREAD"
 
             self.POST_GAME_THREAD = settings.get('POST_GAME_THREAD')
             if self.POST_GAME_THREAD == None: return "Missing POST_GAME_THREAD"
@@ -63,11 +77,36 @@ class Bot:
             self.STICKY = settings.get('STICKY')
             if self.STICKY == None: return "Missing STICKY"
 
-            temp_settings = settings.get('POST_SETTINGS')
-            self.POST_SETTINGS = (temp_settings.get('HEADER'),temp_settings.get('BOX_SCORE'),
-                                    temp_settings.get('LINE_SCORE'),temp_settings.get('SCORING_PLAYS'),
-                                    temp_settings.get('HIGHLIGHTS'))
-            if self.POST_SETTINGS == None: return "Missing POST_SETTINGS"
+            self.SUGGESTED_SORT = settings.get('SUGGESTED_SORT')
+            if self.SUGGESTED_SORT == None: return "Missing SUGGESTED_SORT"
+
+            self.MESSAGE = settings.get('MESSAGE')
+            if self.MESSAGE == None: return "Missing MESSAGE"
+
+            temp_settings = settings.get('PRE_THREAD_SETTINGS')
+            content_settings = temp_settings.get('CONTENT')
+            self.PRE_THREAD_SETTINGS = (temp_settings.get('PRE_THREAD_TAG'),temp_settings.get('PRE_THREAD_TIME'),
+                                            (content_settings.get('PROBABLES'),content_settings.get('FIRST_PITCH'))
+                                       )
+            if self.PRE_THREAD_SETTINGS == None: return "Missing PRE_THREAD_SETTINGS"
+
+            temp_settings = settings.get('THREAD_SETTINGS')
+            content_settings = temp_settings.get('CONTENT')
+            self.THREAD_SETTINGS = (temp_settings.get('THREAD_TAG'),
+                                    (content_settings.get('HEADER'), content_settings.get('BOX_SCORE'),
+                                     content_settings.get('LINE_SCORE'), content_settings.get('SCORING_PLAYS'),
+                                     content_settings.get('HIGHLIGHTS'), content_settings.get('FOOTER'))
+                                 )
+            if self.THREAD_SETTINGS == None: return "Missing THREAD_SETTINGS"
+
+            temp_settings = settings.get('POST_THREAD_SETTINGS')
+            content_settings = temp_settings.get('CONTENT')
+            self.POST_THREAD_SETTINGS = (temp_settings.get('POST_THREAD_TAG'),
+                                    (content_settings.get('HEADER'), content_settings.get('BOX_SCORE'),
+                                     content_settings.get('LINE_SCORE'), content_settings.get('SCORING_PLAYS'),
+                                     content_settings.get('HIGHLIGHTS'), content_settings.get('FOOTER'))
+                                 )
+            if self.POST_THREAD_SETTINGS == None: return "Missing POST_THREAD_SETTINGS"
 
         return 0
 
@@ -79,8 +118,12 @@ class Bot:
             print error_msg
             return
 
-        r = praw.Reddit(user_agent='Baseball-GDT')
-        r.login(self.USERNAME, self.PASSWORD)
+        r = praw.Reddit('OAuth Baseball-GDT-Bot V. 3.0.1'
+                        'https://github.com/mattabullock/Baseball-GDT-Bot')
+        r.set_oauth_app_info(client_id=self.CLIENT_ID,
+                            client_secret=self.CLIENT_SECRET,
+                            redirect_uri=self.REDIRECT_URI)        
+        r.refresh_access_information(self.REFRESH_TOKEN)        
 
         if self.TEAM_TIME_ZONE == 'ET':
             time_info = (self.TEAM_TIME_ZONE,0)
@@ -94,7 +137,8 @@ class Bot:
             print "Invalid time zone settings."
             return
 
-        edit = editor.Editor(time_info, self.POST_SETTINGS)
+        edit = editor.Editor(time_info, self.PRE_THREAD_SETTINGS,
+                self.THREAD_SETTINGS, self.POST_THREAD_SETTINGS)
 
         if self.BOT_TIME_ZONE == 'ET':
             time_before = self.POST_TIME * 60 * 60
@@ -113,12 +157,8 @@ class Bot:
         while True:
             today = datetime.today()
 
-            # getting dirc
             url = "http://gd2.mlb.com/components/game/mlb/"
-            url = url + "year_" + today.strftime("%Y") + "/month_" + today.strftime("%m") + "/day_" + today.strftime("%d") + "/"
-
-            # UNCOMMENT FOR TESTING PURPOSES ONLY
-            # url = url + "year_2015" + "/month_04" + "/day_03/"
+            url = url + "year_" + today.strftime("%Y") + "/month_" + today.strftime("%m") + "/day_" + today.strftime("%d") + "/"         
 
             response = ""
             while not response:
@@ -136,17 +176,66 @@ class Bot:
                     v = v[0:v.index("\"")]
                     directories.append(url + v)
 
+            if self.PREGAME_THREAD and len(directories) > 0:
+                timechecker.pregamecheck(self.PRE_THREAD_SETTINGS[1])
+                title = edit.generate_title(directories[0],"pre")
+                while True:
+                    try:
+                        posted = False
+                        subreddit = r.get_subreddit(self.SUBREDDIT)
+                        for submission in subreddit.get_new():
+                            if submission.title == title:
+                                print "Pregame thread already posted, getting submission..."
+                                submission.edit(edit.generate_pre_code(directories))
+                                posted = True
+                                break
+                        if not posted:
+                            print "Submitting pregame thread..."
+                            sub = r.submit(self.SUBREDDIT, title, edit.generate_pre_code(directories))
+                            print "Pregame thread submitted..."
+                            if self.STICKY:
+                                print "Stickying submission..."
+                                sub.sticky()
+                                print "Submission stickied..."
+                            print "Sleeping for two minutes..."
+                            print datetime.strftime(datetime.today(), "%d %I:%M %p")
+                            time.sleep(5)
+                        break
+                    except Exception, err:
+                        print err
+                        time.sleep(300)
+
             for d in directories:
                 timechecker.gamecheck(d)
-                title = edit.generatetitle(d)
+                title = edit.generate_title(d,"game")
                 if not timechecker.ppcheck(d):
                     while True:
                         check = datetime.today()
                         try:
-                            print "Submitting game thread..."
-                            sub = r.submit(self.SUBREDDIT, title, edit.generatecode(d))
-                            if self.STICKY: sub.sticky()
-                            print "Game thread submitted..."
+                            posted = False
+                            subreddit = r.get_subreddit(self.SUBREDDIT)
+                            for submission in subreddit.get_new():
+                                if submission.title == title:
+                                    print "Thread already posted, getting submission..."
+                                    sub = submission
+                                    posted = True
+                                    break
+                            if not posted:
+                                print "Submitting game thread..."
+                                sub = r.submit(self.SUBREDDIT, title, edit.generate_code(d,"game"))
+                                print "Game thread submitted..."
+                                if self.STICKY:
+                                    print "Stickying submission..."
+                                    sub.sticky()
+                                    print "Submission stickied..."
+                                if self.SUGGESTED_SORT != None:
+                                    print "Setting suggested sort to " + self.SUGGESTED_SORT + "..."
+                                    sub.set_suggested_sort(self.SUGGESTED_SORT)
+                                    print "Suggested sort set..."
+                                if self.MESSAGE:
+                                    print "Messaging Baseballbot..."
+                                    r.send_message('baseballbot', 'Gamethread posted', sub.short_link)
+                                    print "Baseballbot messaged..."
                             print "Sleeping for two minutes..."
                             print datetime.strftime(check, "%d %I:%M %p")
                             time.sleep(5)
@@ -157,7 +246,7 @@ class Bot:
                     pgt_submit = False
                     while True:
                         check = datetime.today()
-                        str = edit.generatecode(d)
+                        str = edit.generate_code(d,"game")
                         while True:
                             try:
                                 sub.edit(str)
@@ -174,6 +263,11 @@ class Bot:
                             check = datetime.today()
                             print datetime.strftime(check, "%d %I:%M %p")
                             print "Game final..."
+                            pgt_submit = True
+                        elif "##COMPLETED EARLY" in str:
+                            check = datetime.today()
+                            print datetime.strftime(check, "%d %I:%M %p")
+                            print "Completed Early..."
                             pgt_submit = True
                         elif "##FINAL: TIE" in str:
                             check = datetime.today()
@@ -198,10 +292,13 @@ class Bot:
                         if pgt_submit:
                             if self.POST_GAME_THREAD:
                                 print "Submitting postgame thread..."
-                                posttitle = edit.generateposttitle(d)
-                                sub = r.submit(self.SUBREDDIT, posttitle, edit.generatecode(d))
-                                if self.STICKY: sub.sticky()
+                                posttitle = edit.generate_title(d,"post")
+                                sub = r.submit(self.SUBREDDIT, posttitle, edit.generate_code(d,"post"))
                                 print "Postgame thread submitted..."
+                                if self.STICKY:
+                                    print "Stickying submission..."
+                                    sub.sticky()
+                                    print "Submission stickied..."
                             break
                         time.sleep(10)
             if datetime.today().day == today.day:
